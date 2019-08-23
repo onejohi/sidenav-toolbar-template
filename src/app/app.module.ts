9932ac00b435007232fbad31b4ee53d51821934f
Onejohi <tonniewanjohi@gmail.com>
2019-08-23 11:46:05 +0300

    Completed the template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  MatSidenavModule, MatButtonModule, MatToolbarModule, MatListModule
+  } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    // import Angular Sidenav and Toolbar
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
